5df0963fc609968cb5717488097f8ac2c99ab5fb
chanwoong <dlcksdnd6785@naver.com>
2021-08-24 00:07:13 +0900

    지연로딩과 즉시로딩

--- src/main/java/helloJPA/domain/Member.java
+++ src/main/java/helloJPA/domain/Member.java
@@ -7,13 +7,13 @@ import javax.persistence.*;
 
 @Entity
 @Getter
-public class Member {
+public class Member extends BaseEntity{
     @Id @GeneratedValue
     @Column(name = "USERNAME")
     private Long id;
     
     //외래키 매핑을 통한 연관간계 매핑!!
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY) //  Team 을 proxy 객체 조회
     @JoinColumn(name = "TEAM_ID")
     private Team team;
 
