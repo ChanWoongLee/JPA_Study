436b674b4d88a1d842f395ac8c054a90810c47b2
chanwoong <dlcksdnd6785@naver.com>
2021-08-27 01:20:05 +0900

    값타입 컬렉션 실습

--- src/main/java/helloJPA/domain/Member.java
+++ src/main/java/helloJPA/domain/Member.java
@@ -5,22 +5,40 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter
-public class Member extends BaseEntity{
-    @Id @GeneratedValue
-    @Column(name = "USERNAME")
+public class Member extends BaseEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
-    
+
     //외래키 매핑을 통한 연관간계 매핑!!
     @ManyToOne(fetch = FetchType.LAZY) //  Team 을 proxy 객체 조회
     @JoinColumn(name = "TEAM_ID")
     private Team team;
 
+    @Embedded
+    private Address homeAddress;
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = "ADDRESS", joinColumns = @JoinColumn(name="MEMBER_ID"))
+    private List<Address> addressHistory = new ArrayList<>();
+
+
     private String userName;
 
-    private void changeTeam(Team team){
+    private void changeTeam(Team team) {
         this.team = team;
         team.getMembers().add(this);
     }
